1db4a56ad3fb70a0939058b8c76ea48fd3bd277f
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-04-05 00:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (49.50%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        if(s=="") return 0;
         int length = 0;
         int maxLength = 0;
-        for(char c : s){
-            if(c == '('){
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
                 length++;
                 maxLength = max(maxLength, length);
             }
-            else if(c == ')'){
+            else if(s[i] == ')'){
                 length--;
             }
         
